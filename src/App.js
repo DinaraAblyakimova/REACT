52388892b6cd668104aa40b1ec08648f844e2165
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-02-21 13:18:02 +0300

    20

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 
 function App() {
-	const li1 = <li>text1</li>;
-	const li2 = <li>text2</li>;
-	const li3 = <li>text3</li>;
-
-	return <main>
-	{li1}{li2}{li3}
-	</main>;
+	const items =<><li>text1</li><li>text2</li><li>text3</li></>;
+	
+	return <ul>
+		{items}
+	</ul>
 }
 
 export default App;
