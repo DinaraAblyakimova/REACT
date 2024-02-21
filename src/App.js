6c99b0b76e2d3fd993504b7fe3cb38faf875c10a
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-02-21 13:15:50 +0300

    19

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 
 function App() {
-	const attr = 'block';
-	
-	return <div id ={attr}>
-		text
-	</div>;
+	const li1 = <li>text1</li>;
+	const li2 = <li>text2</li>;
+	const li3 = <li>text3</li>;
+
+	return <main>
+	{li1}{li2}{li3}
+	</main>;
 }
 
 export default App;
