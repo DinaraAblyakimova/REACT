871c682b449ac04b389240c503ff245f21d0c897
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-02-21 13:48:30 +0300

    30

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 
 function App() {
-	const isAuth = true;
-  
-  return <div>
-   {isAuth && <p>text</p>}
-  </div>;
+ const isAuth = false;
+ 
+ return <div>
+  {!isAuth && <p>пожалуйста, авторизуйтесь</p>}
+ </div>;
+
 
 
 }
