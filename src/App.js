5e61620b08b874ba26babe14b2480fc4709c36da
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2024-02-21 13:39:07 +0300

    26

--- src/App.js
+++ src/App.js
@@ -2,41 +2,23 @@ import React from 'react';
 
 
 function App() {
-	return <div>
-		<ul>
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-			<li>4</li>
-			<li>5</li>
-			<li>6</li>
-			<li>7</li>
-			<li>8</li>
-			<li>9</li>
-			<li>10</li>
-		</ul>
+	let text;
+	const isAdult = true;
+	if (isAdult) {
+		text = <p>Добро пожаловать на сайт </p>
+		}
+		else {
+			text = <p>Вам нет 18 </p>
+		}
 	
+	return (
+	  <div>
+		{text}
+	  </div>
+	)
+  }
+  
 
-		<table>
-			<tbody>
-				<tr>
-					<td>1</td>
-					<td>2</td>
-					<td>3</td>
-				</tr>
-				<tr>
-					<td>4</td>
-					<td>5</td>
-					<td>6</td>
-				</tr>
-				<tr>
-					<td>7</td>
-					<td>8</td>
-					<td>9</td>
-				</tr>
-			</tbody>
-		</table>
-		</div>;
-}
+
 
 export default App;
